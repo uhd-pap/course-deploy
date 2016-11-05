4fa598c8d1aaf68bbae53df43871d4ca47e6d6b8
Nils Fischer <n.fischer@viwid.com>
2016-11-05 11:20:23 +0100

    prepared for deploy

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ../../
-rm -rf release/
-nbgrader assign --create --AssignApp.config_file=source/course/nbgrader_config.py
-cd release/course/
-git commit -a -m"prepared for deploy"
-git push --force deploy master
